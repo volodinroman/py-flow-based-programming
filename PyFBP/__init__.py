c572c88ba42e0c70e5e38b9c0ed1672f6823c96a
Roman Volodin <volodinroman1988@gmail.com>
2019-09-19 17:47:03 -0700

    Update __init__.py

--- PyFBP/__init__.py
+++ PyFBP/__init__.py
@@ -1,1 +1,11 @@
 
+import sys
+import os
+
+project_root  = os.path.dirname(os.path.abspath(__file__))
+project_nodes = os.path.join(project_root, "nodes")
+
+sys.path.append(project_root)
+sys.path.append(project_nodes)
+
+
